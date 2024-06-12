663f96e93693838ff4668a9b2c45b7c396fa0964
suraj-2021 <78522188+suraj-2021@users.noreply.github.com>
2024-06-12 09:20:44 +0530

    Update django_intermeadiatory_model1.py

--- django_intermeadiatory_model1.py
+++ django_intermeadiatory_model1.py
@@ -6,11 +6,11 @@ class Mango(models.Model):
 
 class Person(models.Model):
     name = models.CharField(max_length=80)
-    # Removed the mango_color ForeignKey, as it's redundant with the M2M relationship
+  
     bought = models.ManyToManyField('Mango', through='Purchase')
 
 class Purchase(models.Model):
     mango = models.ForeignKey('Mango', on_delete=models.CASCADE)
     person = models.ForeignKey('Person', on_delete=models.CASCADE)
     date = models.DateField()
-    # Consider if you need the 'friends' field here or if it should be part of a different model/relationship
+   
